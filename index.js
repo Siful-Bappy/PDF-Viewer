b466791a9e4e21af407e86166d4d864c65b1e6a9
Siful-Bappy <bappy.ac.bd@gmail.com>
2022-11-21 14:44:29 +0900

    add pdf.js and pdf.worker.js file

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
-const pdf = 'document.pdf';
+const pdf = "document.pdf";
 
-const pageNum = document.querySelector('#page_num');
-const pageCount = document.querySelector('#page_count');
-const currentPage = document.querySelector('#current_page');
-const previousPage = document.querySelector('#prev_page');
-const nextPage = document.querySelector('#next_page');
-const zoomIn = document.querySelector('#zoom_in');
-const zoomOut = document.querySelector('#zoom_out');
+const pageNum = document.querySelector("#page_num");
+const pageCount = document.querySelector("#page_count");
+const currentPage = document.querySelector("#current_page");
+const previousPage = document.querySelector("#prev_page");
+const nextPage = document.querySelector("#next_page");
+const zoomIn = document.querySelector("#zoom_in");
+const zoomOut = document.querySelector("#zoom_out");
 
 const initialState = {
-    pdfDoc: null,
-    currentPage: 1,
-    pageCount: 0,
-    zoom: 1
-}
+  pdfDoc: null,
+  currentPage: 1,
+  pageCount: 0,
+  zoom: 1,
+};
+
+pdfjsLib
+  .getDocument(pdf)
+  .promise.then((data) => {
+    initialState.pdfDoc = data;
+    // console.log("pdfDocument", initialState.pdfDoc);
+
+    pageCount.textContent = initialState.pdfDoc.numPages;
+    // console.log(pageCount.textContent);
+    //   renderPage();
+  })
+  .catch((err) => {
+    alert(err.message);
+  });
+
+// console.log(pdf)
+// const loadingTask = pdfjsLib.getDocument(pdf);
+// console.log(loadingTask);
